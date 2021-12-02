3460266a325d09e66ca6906547fa0b1f24286d80
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-12-02 20:18:08 +0100

    032_unreachable: Adjust switch var type and handle all possibilities
    Yeah, the exercise wanted me to add an else statement once again, but I
    think this is a much nicer way to complete it.

--- exercises/032_unreachable.zig
+++ exercises/032_unreachable.zig
@@ -20,12 +20,15 @@
 const std = @import("std");
 
 pub fn main() void {
-    const operations = [_]u8{ 1, 1, 1, 3, 2, 2 };
+    const operations = [_]u2{ 1, 1, 1, 3, 2, 2 };
 
     var current_value: u32 = 0;
 
     for (operations) |op| {
         switch (op) {
+            0 => {
+                unreachable;
+            },
             1 => {
                 current_value += 1;
             },
